c67894b05ffb1e8e120cde949ce5b0348d147eac
Matthew.Krol <mkrol@verge-solutions.com>
2014-07-12 23:00:08 -0500

    More Scaffolding

---
+++ MRU.Web/MRU.Data/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRU.Data
+{
+    public class Class1
+    {
+    }
+}
