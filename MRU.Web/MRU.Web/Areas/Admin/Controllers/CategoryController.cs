3a0e1a4367e4f37da9725f85080892b8d1f7787e
Matthew.Krol <mkrol@verge-solutions.com>
2014-07-21 07:33:23 -0500

    Category

---
+++ MRU.Web/MRU.Web/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MRU.Web.Models.Category;
+
+namespace MRU.Web.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Admin/Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View("_Add");
+        }
+
+        [HttpPost]
+        public ActionResult Add(CategoryModel model)
+        {
+            MRU.Data.Category repo = new Data.Category();
+            repo.Add(model);
+            return null;
+        }
+    }
+}
