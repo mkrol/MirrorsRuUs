4fd94976496841cc611064bee5933656f24eef67
Matthew.Krol <mkrol@verge-solutions.com>
2014-08-11 08:54:36 -0500

    product Model changes

--- MRU.Web/MRU.Web.Models/Product/ProductModel.cs
+++ MRU.Web/MRU.Web.Models/Product/ProductModel.cs
@@ -5,16 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 using MRU.Web.Models.Category;
 using NPoco;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace MRU.Web.Models.Product
 {
     [TableName("Product")]
     public class ProductModel : AModel 
     {
+        [Required]
+        [DataType(DataType.Text)]
         public string Title { get; set; }
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [Display(Name="Short Description")]
         public string ShortDescription { get; set; }
+        [DataType(DataType.Upload)]
         public string Image { get; set; }
+        [DataType(DataType.Currency)]
         public double Price { get; set; }
         public double Height { get; set; }
         public double Width { get; set; }
